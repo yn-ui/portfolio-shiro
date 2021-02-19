bc246641eb269dd7816b244c9b3261a6a846f132
yn-ui <72715705+yn-ui@users.noreply.github.com>
2021-02-19 15:11:32 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function(){
+    
+    $('#main-header-p').click(function(){
+        $('#main-modal').fadeIn();
+    });
+
+    $('#main-modal-item-p').click(function(){
+        $('#main-modal').fadeOut();
+    });
+
+});
